c4d10600f7b639824f4a1f4208f8a457eea628f4
mle <mle@whitestein.com>
2021-08-19 23:56:55 +0200

    Add ET/ES/EH inverters constants dictionaries
    Add constants dictionaries (code to label) used by the ET and ES
    inverter families.
    Add modbus CRC-16 table helper function to be used by ET/EH protocols.

--- goodwe/utils.py
+++ goodwe/utils.py
@@ -13,3 +13,19 @@ def get_int_from_buffer(buffer: io.BytesIO, seek_pos: int, size: int, byteorder:
     """Retrieve int value from buffer at given position"""
     buffer.seek(seek_pos)
     return int.from_bytes(buffer.read(size), byteorder=byteorder)
+
+
+def create_crc16_table() -> tuple:
+    """Construct (modbus) CRC-16 table"""
+    table = []
+    for i in range(256):
+        data = i << 1
+        crc = 0
+        for _ in range(8, 0, -1):
+            data >>= 1
+            if (data ^ crc) & 0x0001:
+                crc = (crc >> 1) ^ 0xA001
+            else:
+                crc >>= 1
+        table.append(crc)
+    return tuple(table)
