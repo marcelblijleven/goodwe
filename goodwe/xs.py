ece3daf91f1605ec9209f261d7343e1eed815c6b
mle <mle@whitestein.com>
2021-08-21 18:30:47 +0200

    Switch XS processor to used DT processed sensor map
    Let the DT inverter code decode the response and construct the XS processor ProcessorResult from the sensor map

--- goodwe/xs.py
+++ goodwe/xs.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Callable
 
+from goodwe.dt import DT
 from goodwe.exceptions import InvalidDataException
 from goodwe.processor import ProcessorResult, AbstractDataProcessor
 from goodwe.utils import *
@@ -9,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 
 class GoodWeXSProcessor(AbstractDataProcessor):
-    _buffer: io.BytesIO
     _validator: Callable[[bytes], bool]
     _use_validator: bool
 
@@ -19,79 +19,29 @@ class GoodWeXSProcessor(AbstractDataProcessor):
             logger.debug(f'received invalid data: {data}')
             raise InvalidDataException
 
-        with io.BytesIO(data[5:]) as buffer:
-            self._buffer = buffer
-            result = ProcessorResult(
-                date=self._get_date(),
-                volts_dc=self._get_volts_dc(),
-                current_dc=self._get_current_dc(),
-                volts_ac=self._get_volts_ac(),
-                current_ac=self._get_current_ac(),
-                frequency_ac=self._get_frequency_ac(),
-                generation_today=self._get_generation_today(),
-                generation_total=self._get_generation_total(),
-                rssi=self._get_rssi(),
-                operational_hours=self._get_operational_hours(),
-                temperature=self._get_temperature(),
-                power=self._get_power(),
-                status=self._get_status(),
-            )
+        sensors = DT._map_response(data[5:-2], DT.sensors())
 
-        return result
+        return ProcessorResult(
+            date=sensors['timestamp'],
+            volts_dc=sensors['vpv1'],
+            current_dc=sensors['ipv1'],
+            volts_ac=sensors['vgrid1'],
+            current_ac=sensors['igrid1'],
+            frequency_ac=sensors['fgrid1'],
+            generation_today=sensors['e_day'],
+            generation_total=sensors['e_total'],
+            rssi=self._get_rssi(data), # this is just response checksum
+            operational_hours=sensors['h_total'],
+            temperature=sensors['temperature'],
+            power=sensors['ppv'],
+            status=sensors['work_mode_label'])
 
     def set_validator(self, validator_fn: Callable[[bytes], bool]):
         """Set a validator for the processor to use when processing data from the inverter"""
         self._validator = validator_fn
         self._use_validator = self._validator is not None
 
-    def _get_date(self) -> datetime:
-        """Retrieve time stamp from GoodWe data"""
-        return read_datetime(self._buffer, 0)
-
-    def _get_volts_dc(self) -> float:
-        """Retrieve volts_dc from GoodWe data"""
-        return read_voltage(self._buffer, 6)
-
-    def _get_current_dc(self) -> float:
-        """Retrieve current_dc from GoodWe data"""
-        return read_current(self._buffer, 8)
-
-    def _get_volts_ac(self) -> float:
-        """Retrieve volts_ac from GoodWe data"""
-        return read_voltage(self._buffer, 36)
-
-    def _get_current_ac(self) -> float:
-        """Retrieve current_ac from GoodWe data"""
-        return read_voltage(self._buffer, 42)
-
-    def _get_frequency_ac(self) -> float:
-        """Retrieve frequency_ac from GoodWe data"""
-        return read_freq(self._buffer, 48)
-
-    def _get_generation_today(self) -> float:
-        """Retrieve generation_today from GoodWe data"""
-        return read_power_k2(self._buffer, 88)
-
-    def _get_generation_total(self) -> float:
-        """Retrieve generation_total from GoodWe data"""
-        return read_power_k2(self._buffer, 92)
-
-    def _get_rssi(self) -> float:
+    def _get_rssi(self, data) -> float:
         """Retrieve rssi from GoodWe data"""
-        return read_bytes2(self._buffer, 144)
-
-    def _get_operational_hours(self) -> float:
-        """Retrieve operational_hours from GoodWe data"""
-        return read_bytes2(self._buffer, 96)
-
-    def _get_temperature(self) -> float:
-        """Retrieve rssi from GoodWe data"""
-        return read_temp(self._buffer, 82)
-
-    def _get_power(self) -> float:
-        """Retrieve rssi from GoodWe data"""
-        return read_power2(self._buffer, 56)
-
-    def _get_status(self) -> str:
-        """Retrieve rssi from GoodWe data"""
-        return read_work_mode_dt(self._buffer, 58)
+        with io.BytesIO(data) as buffer:
+            return read_bytes2(buffer, 149)
