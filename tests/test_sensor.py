157e197878cdbbcb0755b532ab1d8336508c4820
Martin Letenay <mle@whitestein.com>
2021-08-29 21:07:28 +0200

    Remove obsolete sign logic from read_current/power
    Remove obsolete sign logic from read_current/power

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,7 +1,7 @@
 import io
 from unittest import TestCase
 
-from goodwe.sensor import read_byte, read_bytes2, read_voltage
+from goodwe.sensor import read_byte, read_bytes2, read_voltage, read_current, read_power
 
 
 class TestUtils(TestCase):
@@ -24,9 +24,29 @@ class TestUtils(TestCase):
         self.assertEqual(read_bytes2(buffer, 1), 1337)
 
     def test_read_voltage(self):
-        data = bytes()
-        data += (21).to_bytes(1, 'big')
-        data += (1337).to_bytes(2, 'big')
+        data = bytes.fromhex("0cfe")
         buffer = io.BytesIO(data)
+        self.assertEqual(read_voltage(buffer), 332.6)
 
-        self.assertEqual(read_voltage(buffer, 1), 133.7)
+        data = bytes.fromhex("1f64")
+        buffer = io.BytesIO(data)
+        self.assertEqual(read_voltage(buffer), 803.6)
+
+    def test_read_current(self):
+        data = bytes.fromhex("0031")
+        buffer = io.BytesIO(data)
+        self.assertEqual(read_current(buffer), 4.9)
+
+        data = bytes.fromhex("ff9e")
+        buffer = io.BytesIO(data)
+        self.assertEqual(read_current(buffer), -9.8)
+
+    def test_read_power(self):
+        data = bytes.fromhex("0000069f")
+        buffer = io.BytesIO(data)
+        self.assertEqual(read_power(buffer), 1695)
+
+        data = bytes.fromhex("fffffffd")
+        buffer = io.BytesIO(data)
+        self.assertEqual(read_power(buffer), -3)
+
