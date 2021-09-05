7803a642b9f5712dd8e6d59438960151d6a29586
mle <mle@whitestein.com>
2021-09-05 23:32:16 +0200

    Improve response length check vs request parameter
    When processing response to modbus read request, consider also the number of modbus registers requested vs. the response payload length.
    (Sometimes it can happen, the response is syntactically correct, but was meant for different request)

--- tests/test_modbus.py
+++ tests/test_modbus.py
@@ -5,24 +5,34 @@ from goodwe.modbus import create_modbus_request, validate_modbus_response
 
 class TestModbus(TestCase):
 
+    def assert_response_ok(self, response: str, cmd: int, value: int):
+        self.assertTrue(validate_modbus_response(bytes.fromhex(response), cmd, 0, value))
+
+    def assert_response_fail(self, response: str, cmd: int, value: int):
+        self.assertFalse(validate_modbus_response(bytes.fromhex(response), cmd, 0, value))
+
     def test_create_modbus_request(self):
         request = create_modbus_request(0xf7, 0x3, 0x88b8, 0x0021)
         self.assertEqual(bytes.fromhex('f70388b800213ac1'), request)
 
     def test_validate_modbus_read_response(self):
-        self.assertTrue(validate_modbus_response(bytes.fromhex('aa55f7030501020304053347')))
+        self.assert_response_ok('aa55f7030401020304cd33', 0x03, 2)
         # some garbage after response end
-        self.assertTrue(validate_modbus_response(bytes.fromhex('aa55f7030501020304053347ffffff')))
+        self.assert_response_ok('aa55f7030401020304cd33ffffff', 0x03, 2)
         # length too short
-        self.assertFalse(validate_modbus_response(bytes.fromhex('aa55f7030501020304')))
+        self.assert_response_fail('aa55f7030401020304', 0x03, 2)
         # wrong checksum
-        self.assertFalse(validate_modbus_response(bytes.fromhex('aa55f7030501020304053346')))
+        self.assert_response_fail('aa55f70304010203043346', 0x03, 2)
+        # failure code
+        self.assert_response_fail('aa55f783040102030405b35e', 0x03, 2)
+        # unexpected message length
+        self.assert_response_fail('aa55f70306010203040506b417', 0x03, 2)
 
     def test_validate_modbus_write_response(self):
-        self.assertTrue(validate_modbus_response(bytes.fromhex('aa55f706b12c00147ba6')))
+        self.assert_response_ok('aa55f706b12c00147ba6', 0x06, 0)
         # some garbage after response end
-        self.assertTrue(validate_modbus_response(bytes.fromhex('aa55f706b12c00147ba6ffffff')))
+        self.assert_response_ok('aa55f706b12c00147ba6ffffff', 0x06, 0)
         # length too short
-        self.assertFalse(validate_modbus_response(bytes.fromhex('aa55f7066b12')))
+        self.assert_response_fail('aa55f7066b12', 0x06, 0)
         # wrong checksum
-        self.assertFalse(validate_modbus_response(bytes.fromhex('aa55f706b12c00147ba7')))
+        self.assert_response_fail('aa55f706b12c00147ba7', 0x06, 0)
