5ba0d18fe172101a1855023c883ba0bb4020a918
mle <mle@whitestein.com>
2024-05-08 19:29:56 +0200

    Add pip warning to inverter_check.py

--- tests/inverter_check.py
+++ tests/inverter_check.py
@@ -2,9 +2,12 @@
 import asyncio
 import logging
 import sys
+from importlib.metadata import version
 
 # Force the local files, not pip installed lib
+sys.path.insert(0, '../goodwe')
 sys.path.insert(0, '../../goodwe')
+
 import goodwe
 
 logging.basicConfig(
@@ -15,6 +18,19 @@ logging.basicConfig(
 
 if sys.platform.startswith('win'):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+module_ver = None
+try:
+    module_ver = version('goodwe')
+except ModuleNotFoundError:
+    pass
+
+if module_ver:
+    print("WARNING !!!")
+    print("==============================")
+    print(f"You are executing code with installed pip version goodwe:{module_ver}")
+    print("You are not testing the local files, if that was what you meant !!!")
+    print("==============================")
 
 # Set the appropriate IP address
 IP_ADDRESS = "192.168.2.14"
